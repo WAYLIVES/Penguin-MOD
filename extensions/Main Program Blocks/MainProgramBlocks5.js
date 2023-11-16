d9a80f7fac5d3180cb1277aa99c5505a99d78f4c
Richard Melentyev <130656326+WAYLIVES@users.noreply.github.com>
2023-11-16 20:02:11 +0300

    Update MainProgramBlocks5.js

--- extensions/Main Program Blocks/MainProgramBlocks5.js
+++ extensions/Main Program Blocks/MainProgramBlocks5.js
@@ -165,6 +165,16 @@
     const forceUpdateDirectionAndScale = (target) => {
       target.setDirection(target.direction);
     };
+
+    const requireNonPackagedRuntime = (blockName) => {
+      if (Scratch.vm.runtime.isPackaged) {
+        alert(
+          `To use the Looks+ ${blockName} block, the creator of the packaged project must uncheck "Remove raw asset data after loading to save RAM" under advanced settings in the packager.`
+        );
+        return false;
+      }
+      return true;
+    };
 /* ________________________________________________________________________________________ */
 
 
@@ -179,8 +189,8 @@
         id: "MainProgramBlocks",
         name: "MPB",
         color1: "#BF6E8B",
-        color2: "#A0536F",
-        color3: "#A0536F",
+        color2: "#AD5D7A",
+        color3: "#F29CBB",
         menuIconURI: menuIconURI,
         
         blocks: [
@@ -383,6 +393,7 @@
           },
 
           "---",
+          
 
         // Всё для адаптации:
           {
@@ -443,6 +454,20 @@
             filter: [Scratch.TargetType.SPRITE],
             disableMonitor: true,
           },
+          {
+            opcode: "costumeAttribute",
+            blockType: Scratch.BlockType.REPORTER,
+            text: "[ATTRIBUTE] of [COSTUME]",
+            arguments: {
+              ATTRIBUTE: {
+                type: Scratch.ArgumentType.STRING,
+                menu: "costumeAttribute",
+              },
+              COSTUME: {
+                type: Scratch.ArgumentType.COSTUME,
+              },
+            },
+          },
         ],
 /* ________________________________________________________________________________________ */
 
@@ -455,6 +480,7 @@
         menus: {
           
         // Элементы управления:
+          
           MOVE: {
             acceptReporters: true,
             items: [
@@ -501,8 +527,27 @@
             ],
           },
           
+
+          
           
         // Всё для адаптации:
+          
+          costumeAttribute: {
+            acceptReporters: false,
+            items: [ 
+              "width", 
+              "height", 
+              "format", 
+              {
+                text: "rotation center x",
+                value: "rotationCenterX",
+              },
+              {
+                text: "rotation center y",
+                value: "rotationCenterY",
+              },
+            ],
+          },
         },
       };
     }
@@ -653,6 +698,72 @@
     }
     getY(args, util) {
       return util.target[STRETCH_Y];
+    }
+
+    costumeAttribute(args, util) {
+      const costumeIndex = this.getCostumeInput(args.COSTUME, util.target);
+      const costume = util.target.sprite.costumes[costumeIndex];
+      if (!costume) {
+        console.error("Costume doesn't exist");
+        return 0;
+      }
+
+      const attribute = args.ATTRIBUTE;
+      if (attribute === "width") {
+        return Math.ceil(Scratch.Cast.toNumber(costume.size[0]));
+      } else if (attribute === "height") {
+        return Math.ceil(Scratch.Cast.toNumber(costume.size[1]));
+      } else if (attribute === "format") {
+        if (!requireNonPackagedRuntime("costume format")) {
+          return "unknown";
+        }
+        return costume.asset.assetType.runtimeFormat;
+      } else if (attribute === "rotationCenterX") {
+        return costume.rotationCenterX;
+      } else if (attribute === "rotationCenterY") {
+        return costume.rotationCenterY;
+      } else {
+        return "";
+      }
+    }
+    getCostumeInput(costume, target) {
+      if (typeof costume === "number") {
+        costume = Math.round(costume - 1);
+        if (costume === Infinity || costume === -Infinity || !costume) {
+          costume = 0;
+        }
+        costume = this.wrapClamp(costume, 0, target.sprite.costumes.length - 1);
+        return costume;
+      } else {
+        return target.getCostumeIndexByName(Scratch.Cast.toString(costume));
+      }
+    }
+    getSprites() {
+      const spriteNames = [];
+      const targets = Scratch.vm.runtime.targets;
+      const myself = Scratch.vm.runtime.getEditingTarget().getName();
+      for (let index = 1; index < targets.length; index++) {
+        const target = targets[index];
+        if (target.isOriginal) {
+          const targetName = target.getName();
+          if (targetName === myself) {
+            spriteNames.unshift({
+              text: "this sprite",
+              value: targetName,
+            });
+          } else {
+            spriteNames.push({
+              text: targetName,
+              value: targetName,
+            });
+          }
+        }
+      }
+      if (spriteNames.length > 0) {
+        return spriteNames;
+      } else {
+        return [{ text: "", value: 0 }]; //this should never happen but it's a failsafe
+      }
     }
 /* ________________________________________________________________________________________ */
 
