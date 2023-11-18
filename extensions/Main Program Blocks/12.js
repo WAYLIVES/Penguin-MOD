1108b97f6724c2eedc51c55c7e73934cb3a80bae
Richard Melentyev <130656326+WAYLIVES@users.noreply.github.com>
2023-11-18 14:01:45 +0300

    Update 12.js

--- extensions/Main Program Blocks/12.js
+++ extensions/Main Program Blocks/12.js
@@ -9,22 +9,7 @@
   if (!Scratch.extensions.unsandboxed) {
     throw new Error("More Motion must run unsandboxed");
   }
-
-  // @ts-expect-error - not typed yet
-  const Rectangle = Scratch.vm.renderer.exports.Rectangle;
-
-  const requireNonPackagedRuntime = (blockName) => {
-    if (Scratch.vm.runtime.isPackaged) {
-      alert(
-        `To use the Looks+ ${blockName} block, the creator of the packaged project must uncheck "Remove raw asset data after loading to save RAM" under advanced settings in the packager.`
-      );
-      return false;
-    }
-    return true;
-  };
-
-
-
+  
 
   const anchor_position = Symbol('anchor.position');
 
@@ -39,10 +24,7 @@
 
   const vm = Scratch.vm;
 
-  /**
-  * @param {VM.RenderedTarget} target
-  * @param {VM.RenderedTarget} [originalTarget] If target is a clone, the original to copy from.
-  */
+  
   const implementAnchorForTarget = (target, originalTarget) => {
     if (anchor_position in target) {
       return;
@@ -60,18 +42,18 @@
     target[anchor_retreat] = originalTarget ? originalTarget[anchor_retreat] : true;
   };
 
-  vm.runtime.targets.forEach((target) => implementAnchorForTarget(target));
+  Scratch.vm.runtime.targets.forEach((target) => implementAnchorForTarget(target));
 
-  vm.runtime.on("targetWasCreated", (target, originalTarget) =>
+  Scratch.vm.runtime.on("targetWasCreated", (target, originalTarget) =>
     implementAnchorForTarget(target, originalTarget)
   );
 
-  vm.runtime.on("PROJECT_LOADED", () => {
+  Scratch.vm.runtime.on("PROJECT_LOADED", () => {
     vm.runtime.targets.forEach((target) => implementAnchorForTarget(target));
   });
 
-  vm.runtime.on("BEFORE_EXECUTE", () => {
-    vm.runtime.targets.forEach((target) => {
+  Scratch.vm.runtime.on("BEFORE_EXECUTE", () => {
+    Scratch.vm.runtime.targets.forEach((target) => {
       if (target[anchor_updateAnchorEveryFrame]) {
         _updateAnchor({ target: target });
       }
@@ -81,8 +63,8 @@
 
   const _touchingEdge = (target, axis) => {
     if (target.renderer) {
-      const stageWidth = vm.runtime.stageWidth;
-      const stageHeight = vm.runtime.stageHeight;
+      const stageWidth = Scratch.vm.runtime.stageWidth;
+      const stageHeight = Scratch.vm.runtime.stageHeight;
 
       const bounds = target.getBounds();
 
@@ -101,13 +83,6 @@
   const _move = (target, info, x, y, resolution = 1, retreat = true) => {
     const maxAttempts = (info.size.x + info.size.y) * resolution;
     let attempts = maxAttempts;
-
-    /*
-    while (target.isTouchingObject('_edge_') && attempts > 0) {
-      target.setXY(target.x + x / resolution, target.y + y / resolution);
-      attempts -= 1;
-    }
-    */
 
     if (x != 0) {
       attempts = maxAttempts;
@@ -190,80 +165,49 @@
 
     switch (target[anchor_position]) {
       case 'top left':
-        target.setXY(
-          vm.runtime.stageWidth / -2,
-          vm.runtime.stageHeight / 2
-        );
-
+        target.setXY( Scratch.vm.runtime.stageWidth / -2, Scratch.vm.runtime.stageHeight / 2 );
         _move(target, info, 1, -1, target[anchor_resolution], target[anchor_retreat]);
-
         break;
+        
       case 'top center':
-        target.setXY(
-          0,
-          vm.runtime.stageHeight / 2
-        );
-
+        target.setXY( 0, Scratch.vm.runtime.stageHeight / 2 );
         _move(target, info, 0, -1, target[anchor_resolution], target[anchor_retreat]);
-
         break;
+        
       case 'top right':
-        target.setXY(
-          vm.runtime.stageWidth / 2,
-          vm.runtime.stageHeight / 2
-        );
-
+        target.setXY( Scratch.vm.runtime.stageWidth / 2, Scratch.vm.runtime.stageHeight / 2 );
         _move(target, info, -1, -1, target[anchor_resolution], target[anchor_retreat]);
-
         break;
+        
       case 'middle left':
-        target.setXY(
-          vm.runtime.stageWidth / -2,
-          0
-        );
-
+        target.setXY( Scratch.vm.runtime.stageWidth / -2, 0 );
         _move(target, info, 1, 0, target[anchor_resolution], target[anchor_retreat]);
-
         break;
+        
       case 'middle center':
-        target.setXY(0, 0);
+        target.setXY( 0, 0 );        
         break;
+        
       case 'middle right':
-        target.setXY(
-          vm.runtime.stageWidth / 2,
-          0
-        );
-
+        target.setXY( Scratch.vm.runtime.stageWidth / 2, 0 );
         _move(target, info, -1, 0, target[anchor_resolution], target[anchor_retreat]);
-
         break;
+        
       case 'bottom left':
-        target.setXY(
-          vm.runtime.stageWidth / -2,
-          vm.runtime.stageHeight / -2
-        );
-
+        target.setXY( Scratch.vm.runtime.stageWidth / -2, Scratch.vm.runtime.stageHeight / -2 );
         _move(target, info, 1, 1, target[anchor_resolution], target[anchor_retreat]);
-
         break;
+        
       case 'bottom center':
-        target.setXY(
-          0,
-          vm.runtime.stageHeight / -2
-        );
-
+        target.setXY( 0, Scratch.vm.runtime.stageHeight / -2 );
         _move(target, info, 0, 1, target[anchor_resolution], target[anchor_retreat]);
-
         break;
+        
       case 'bottom right':
-        target.setXY(
-          vm.runtime.stageWidth / 2,
-          vm.runtime.stageHeight / -2
-        );
-
+        target.setXY( Scratch.vm.runtime.stageWidth / 2, Scratch.vm.runtime.stageHeight / -2 );
         _move(target, info, -1, 1, target[anchor_resolution], target[anchor_retreat]);
-
         break;
+        
       default:
         break;
     }
@@ -294,7 +238,7 @@
           },
           {
             filter: [Scratch.TargetType.SPRITE],
-            opcode: "setXY",
+            opcode: "setXYa",
             blockType: Scratch.BlockType.COMMAND,
             text: "выравнить костюм [ALIGN] отступить по x: [AX] y: [AY]",
             arguments: {
@@ -307,20 +251,6 @@
               },
               AY: {
                 type: Scratch.ArgumentType.NUMBER,
-              },
-            },
-          },
-          {
-            opcode: "costumeAttribute",
-            blockType: Scratch.BlockType.REPORTER,
-            text: "[ATTRIBUTE] of [COSTUME]",
-            arguments: {
-              ATTRIBUTE: {
-                type: Scratch.ArgumentType.STRING,
-                menu: "costumeAttribute",
-              },
-              COSTUME: {
-                type: Scratch.ArgumentType.COSTUME,
               },
             },
           },
@@ -420,86 +350,15 @@
     }
 
 
-    
-    costumeAttribute(args, util) {
-      const costumeIndex = this.getCostumeInput(args.COSTUME, util.target);
-      const costume = util.target.sprite.costumes[costumeIndex];
-      if (!costume) {
-        console.error("Costume doesn't exist");
-        return 0;
-      }
-
-      const attribute = args.ATTRIBUTE;
-      if (attribute === "width") {
-        return Math.ceil(Scratch.Cast.toNumber(costume.size[0]));
-      } else if (attribute === "height") {
-        return Math.ceil(Scratch.Cast.toNumber(costume.size[1]));
-      } else if (attribute === "format") {
-        if (!requireNonPackagedRuntime("costume format")) {
-          return "unknown";
-        }
-        return costume.asset.assetType.runtimeFormat;
-      } else if (attribute === "rotationCenterX") {
-        return costume.rotationCenterX;
-      } else if (attribute === "rotationCenterY") {
-        return costume.rotationCenterY;
-      } else {
-        return "";
-      }
-    }
-    getCostumeInput(costume, target) {
-      if (typeof costume === "number") {
-        costume = Math.round(costume - 1);
-        if (costume === Infinity || costume === -Infinity || !costume) {
-          costume = 0;
-        }
-        costume = this.wrapClamp(costume, 0, target.sprite.costumes.length - 1);
-        return costume;
-      } else {
-        return target.getCostumeIndexByName(Scratch.Cast.toString(costume));
-      }
-    }
-    getSprites() {
-      const spriteNames = [];
-      const targets = Scratch.vm.runtime.targets;
-      const myself = Scratch.vm.runtime.getEditingTarget().getName();
-      for (let index = 1; index < targets.length; index++) {
-        const target = targets[index];
-        if (target.isOriginal) {
-          const targetName = target.getName();
-          if (targetName === myself) {
-            spriteNames.unshift({
-              text: "this sprite",
-              value: targetName,
-            });
-          } else {
-            spriteNames.push({
-              text: targetName,
-              value: targetName,
-            });
-          }
-        }
-      }
-      if (spriteNames.length > 0) {
-        return spriteNames;
-      } else {
-        return [{ text: "", value: 0 }];
-      }
-    }
-
-
-    setXY(args, util) {
+    setXYa(args, util) {
       const x = Scratch.Cast.toNumber(args.AX);
       const y = Scratch.Cast.toNumber(args.AY);
 
-      const spriteNames = [];
-      const targets = Scratch.vm.runtime.targets;
-      const myself = Scratch.vm.runtime.getEditingTarget().getName();
       
       if (args.ALIGN == "center") {
         const center = util.target.setXY( 0, 0 );
       } else if (args.ALIGN == "right") {
-        const right = util.target.setXY( ((Scratch.vm.runtime.stageWidth / 2) - Scratch.vm.anchor.offset.y ) , 0 );
+        const right = util.target.setXY( (Scratch.vm.runtime.stageWidth / 2) , 0 );
       } else if (args.ALIGN == "left") {
         const left = util.target.setXY( (-1 * (Scratch.vm.runtime.stageWidth / 2)) , 0 );
       } else if (args.ALIGN == "top") {
@@ -511,7 +370,8 @@
       } else if (args.ALIGN == "rightBottom") {
         const rightBottom = util.target.setXY( (Scratch.vm.runtime.stageWidth / 2) , (-1 * (Scratch.vm.runtime.stageHeight / 2)) );
       } else if (args.ALIGN == "leftTop") {
-        const leftTop = util.target.setXY( (-1 * (Scratch.vm.runtime.stageWidth / 2)) , (Scratch.vm.runtime.stageHeight / 2) );
+        const leftTop = util.target.setXY( (Scratch.vm.runtime.stageWidth / -2) , (Scratch.vm.runtime.stageHeight / 2) );
+        _move(target, info, 1, -1, target[anchor_resolution], target[anchor_retreat]);
       } else if (args.ALIGN == "leftBottom") {
         const leftBottom = util.target.setXY( (-1 * (Scratch.vm.runtime.stageWidth / 2)) , (-1 * (Scratch.vm.runtime.stageHeight / 2)) );
       }
