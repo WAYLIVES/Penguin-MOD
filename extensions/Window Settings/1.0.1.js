52a91c45213117055b6d15671793490628fdb0bb
Richard Melentyev <130656326+WAYLIVES@users.noreply.github.com>
2023-11-18 20:18:11 +0300

    Update 1.0.1.js

--- extensions/Window Settings/1.0.1.js
+++ extensions/Window Settings/1.0.1.js
@@ -27,6 +27,7 @@
   Scratch.translate.setup({
     
     ru /*_РусскийЯзык_*/: {
+      ExtensionName: "Настройки Окна",
       LabelA: "Window Settings:",
     },
   }); 
@@ -48,7 +49,7 @@
 
 /* ________________________________________________________________________________________ */  
   // иконка меню:
-  const menuIconURI = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwIiBoZWlnaHQ9IjEwMCIgdmlld0JveD0iMCAwIDEwMCAxMDAiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxyZWN0IHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIiByeD0iMTAiIGZpbGw9IiNCRjZFOEIiLz4KPHJlY3QgeD0iMjIiIHk9IjIyIiB3aWR0aD0iNTYiIGhlaWdodD0iNTYiIHJ4PSI2IiBmaWxsPSJ3aGl0ZSIvPgo8cmVjdCB4PSIzMCIgeT0iNDIiIHdpZHRoPSI0MCIgaGVpZ2h0PSIyOCIgcng9IjMiIGZpbGw9IiNCRjZFOEIiLz4KPHJlY3QgeD0iMzYiIHk9IjUwIiB3aWR0aD0iMTgiIGhlaWdodD0iMTQiIHJ4PSIyIiBmaWxsPSJ3aGl0ZSIvPgo8cmVjdCB4PSI1OCIgeT0iNTAiIHdpZHRoPSI2IiBoZWlnaHQ9IjE0IiByeD0iMiIgZmlsbD0id2hpdGUiLz4KPGNpcmNsZSBjeD0iNjUiIGN5PSIzMiIgcj0iNSIgZmlsbD0iI0JGNkU4QiIvPgo8L3N2Zz4K";
+  const menuIconURI = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwIiBoZWlnaHQ9IjEwMCIgdmlld0JveD0iMCAwIDEwMCAxMDAiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxyZWN0IHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIiByeD0iMTAiIGZpbGw9IiNEMTM0MkEiLz4KPHJlY3QgeD0iMjIiIHk9IjIyIiB3aWR0aD0iNTYiIGhlaWdodD0iNTYiIHJ4PSI2IiBmaWxsPSJ3aGl0ZSIvPgo8cmVjdCB4PSIzMCIgeT0iNDIiIHdpZHRoPSI0MCIgaGVpZ2h0PSIyOCIgcng9IjMiIGZpbGw9IiNEMTM0MkEiLz4KPGNpcmNsZSBjeD0iNjUiIGN5PSIzMiIgcj0iNSIgZmlsbD0iI0QxMzQyQSIvPgo8L3N2Zz4K";
   
   // иконки блоков:
 /* ________________________________________________________________________________________ */
@@ -72,10 +73,10 @@
     getInfo() {
       return {              
         id: "WindowSettings",
-        name: "Window Settings",
-        color1: "#D34B2D",
-        color2: "#B73E23",
-        color3: "#000000",
+        name: Scratch.translate({ id: "ExtensionName", default: "Window Settings" }),
+        color1: "#D1342A",
+        color2: "#A92B22",
+        color3: "#7D1F18",
         menuIconURI: menuIconURI,
         
         blocks: [ 
